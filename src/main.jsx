a284cf4f12665e3550aeb7724497a01f027b1beb
abir781 <hasan.abir3176@gmail.com>
2025-06-06 01:20:23 +0600

    basic-route-setup

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Root from './Components/Root.jsx';
+import Home from './Pages/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+    children:[
+      {
+        path:'/',
+        Component:Home,
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
+)
