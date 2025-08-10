3e3b66451f95574db1f9c2688585665ef1391c0b
abir781 <hasan.abir3176@gmail.com>
2025-08-10 11:54:45 +0600

    slider-updated

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -20,19 +20,28 @@ const Banner = () => {
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
     >
-      <SwiperSlide className='text-white'>
-        <img className='h-[70vh] relative ' src="/slider1.jpg" alt="" style={{width:"100%"}} />
+      <SwiperSlide className='text-white '>
+        
+              
+            <img className='h-[70vh] relative ' src="/slider1.jpg" alt="" style={{width:"100%"}} />
+            <div class="absolute inset-0 bg-[linear-gradient(rgba(20,20,20,0.4),rgba(20,20,20,0.4))]"></div>
         <h1 className='text-white text-6xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>Stay Fresh, Stay Ahead</h1>
+
+      
+      
         
         </SwiperSlide>
       <SwiperSlide className='text-white'>
         <img className='h-[70vh] relative ' src="/slider2.jpg" alt="" style={{width:"100%"}}/>
+        <div class="absolute inset-0 bg-[linear-gradient(rgba(20,20,20,0.4),rgba(20,20,20,0.4))]"></div>
         <h1 className='text-white text-6xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center '>Track Smart. Waste Less</h1>
+
         
 
         </SwiperSlide>
       <SwiperSlide className='text-white'>
         <img className='h-[70vh] relative ' src="/slider3.jpg" alt="" style={{width:"100%"}}/>
+        <div class="absolute inset-0 bg-[linear-gradient(rgba(20,20,20,0.4),rgba(20,20,20,0.4))]"></div>
          <h1 className='text-white text-6xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>Know What's in Your Fridge - Before It's Too Late.</h1>
         
         </SwiperSlide>
